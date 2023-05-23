363af7ab0ae4612d5537f238b9ee8ab69c22e53d
Alex <alexs.lecao@gmail.com>
2023-05-23 11:34:50 -0300

    created login methods and validate login

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,12 +1,23 @@
 const express = require('express')
+const auth = require('./auth');
 
-module.exports = function(server) {
+module.exports = function (server) {
 
-    // Definir URL base para todas as rotas 
-    const router = express.Router()
-    server.use('/api', router)
+    /*  Rotas protegidas por Token JWT */
+    const protectedApi = express.Router();
+    server.use('/api', protectedApi);
 
-    // Rotas de Ciclo de Pagamento 
-    const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
+    protectedApi.use(auth);
+
+    const BillingCycle = require('../api/billingCycle/billingCycleService');
+    BillingCycle.register(protectedApi, '/billingCycles');
+
+    /* Rotas Abertas */
+    const openApi = express.Router();
+    server.use('/oapi', openApi);
+
+    const AuthService = require('../api/user/authService');
+    openApi.post('/login', AuthService.login);
+    openApi.post('/sigup', AuthService.sigup);
+    openApi.post('/validateToken', AuthService.validateToken);
 }
